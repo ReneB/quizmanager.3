6b5a83958ccba7405b42b50ea0a72cb7fd08c0c6
Rene van den Berg <rene@ranj.nl>
2016-07-15 11:41:06 +0200

    Add a sensible spec for Question-related behavior

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "callbacks" do
+    context "when a question is created" do
+      context "even if no answers are set to be correct" do
+        let(:answers)   { Array.new(4) do Answer.new end }
+        let(:question)  { Question.new(answers: answers) }
+
+        before :each do
+          question.save
+        end
+
+        it "assumes the first answer is the correct one" do
+          question.correct_answer == answers.first
+        end
+      end
+    end
+  end
 end
