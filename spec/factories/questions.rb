424491c5a10cbdac8c88cb3aff4ea9b3253e75d3
Rene van den Berg <rene@ranj.nl>
2016-07-22 13:38:58 +0200

    Fix a view spec that was a little over-eager looking for fields that don't exist

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,5 +1,15 @@
 FactoryGirl.define do
   factory :question do
     user nil
+    categories do
+      (1..4).to_a.sample.times.map do
+        build(:category)
+      end
+    end
+    answers do
+      4.times.map do
+        build(:answer)
+      end
+    end
   end
 end
--- spec/views/questions/edit.html.erb_spec.rb
+++ spec/views/questions/edit.html.erb_spec.rb
@@ -2,17 +2,12 @@ require 'rails_helper'
 
 RSpec.describe "questions/edit", type: :view do
   before(:each) do
-    @question = assign(:question, Question.create!(
-      :user => nil
-    ))
+    @question = assign(:question, FactoryGirl.create(:question))
   end
 
   it "renders the edit question form" do
     render
 
-    assert_select "form[action=?][method=?]", question_path(@question), "post" do
-
-      assert_select "input#question_user_id[name=?]", "question[user_id]"
-    end
+    assert_select "form[action=?][method=?]", question_path(@question), "post"
   end
 end
