56ca10f7c2acf17faf0718232714936c2c2cee05
Rene van den Berg <rene@ranj.nl>
2016-07-29 10:33:59 +0200

    Fix a test that broke due to the introduction of a new column in the table

--- spec/views/questions/index.html.erb_spec.rb
+++ spec/views/questions/index.html.erb_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe "questions/index", type: :view do
+  let(:question_text) { "This text is being matched in the spec" }
+
   before(:each) do
     assign(:questions, [
-      FactoryGirl.create(:question),
-      FactoryGirl.create(:question)
+      FactoryGirl.create(:question, question_text: question_text),
+      FactoryGirl.create(:question, question_text: question_text)
     ])
   end
 
   it "renders a list of questions" do
     render
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td", text: Regexp.new(question_text), count: 2
   end
 end
