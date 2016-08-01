5e5e33a330773b521f6b4ea5aadb66876aab3e95
Rene van den Berg <rene@ranj.nl>
2016-08-01 15:35:02 +0200

    Add sensible FactoryGirl definition for ContentImage

--- spec/factories/content_images.rb
+++ spec/factories/content_images.rb
@@ -1,4 +1,7 @@
 FactoryGirl.define do
   factory :content_image do
+    filename        { "image.png" }
+    question_image  { File.new("#{Rails.root}/spec/support/fixtures/question_image.png") }
+    answer_image    { File.new("#{Rails.root}/spec/support/fixtures/answer_image.png") }
   end
 end
